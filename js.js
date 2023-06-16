c16ccdf55f4fb0b4e704e72f6302025d6c9a411d
Joseph Kamandy <josephkamandy@gmail.com>
2023-06-15 18:40:09 -0700

    updated home page

--- js.js
+++ js.js
@@ -1,1 +1,33 @@
 console.log("Welcome to Yosuf's Online Portfolio!")
+
+const typewriterElements = document.querySelectorAll('.typewriter');
+
+typewriterElements.forEach((element) => {
+  const text = element.innerText;
+  element.innerHTML = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const letter = text[i];
+    const span = document.createElement('span');
+    span.innerText = letter;
+    span.style.animationDelay = `${i * 0.1}s`;
+    element.appendChild(span);
+  }
+});
+
+const word = document.querySelector('.word');
+const spans = word.querySelectorAll('span');
+
+spans.forEach((span, index) => {
+  span.addEventListener('mouseover', () => {
+    for (let i = 0; i <= index; i++) {
+      spans[i].classList.add('hovered');
+    }
+  });
+
+  span.addEventListener('mouseout', () => {
+    spans.forEach((span) => {
+      span.classList.remove('hovered');
+    });
+  });
+});
